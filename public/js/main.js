cefdb33f5801d73652735d3216fe3d72e7fdc4e7
Masayoshi Mizutani <muret@haeena.net>
2015-01-04 15:55:08 +0900

    init

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var socket = io.connect('http://localhost');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
+});
