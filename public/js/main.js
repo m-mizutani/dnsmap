7a8a93f8f492ed27b7edbc0b083e8c7fc4ac0b70
Masa Mizutani <mizutani@sfc.wide.ad.jp>
2015-01-08 02:42:24 +0900

    added page switcher and about contents

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,20 @@ $(document).ready(function() {
                          '</div>');
     },
   });
-  
+
+  function show_content(c_id) {
+    $('ul.nav li').removeClass('active');
+    $('div.content').addClass('hidden');
+    $('ul.nav li#' + c_id).addClass('active');
+    $('div.content#' + c_id).removeClass('hidden');
+  }
+  var menu = ['home', 'about', 'example'];
+  menu.forEach(function(m) {
+    $('li#' + m).click(function() {
+      show_content(m);
+    });
+  });
+
   /*
   var socket = io.connect('http://localhost');
   socket.on('news', function (data) {
